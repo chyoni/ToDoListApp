cb2f47666c5eead08a7e1cf5969c96f8a649e3c1
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-10-11 11:09:20 +0900

    #01 Init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  TextInputChangeEventData,
+  NativeSyntheticEvent,
+} from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
+  const [isWork, setIsWork] = useState<boolean>(true);
+  const [text, setText] = useState<string>('');
+  const work = (): void => {
+    setIsWork(true);
+  };
+  const travel = (): void => {
+    setIsWork(false);
+  };
+  const onChangeText = (payload: string): void => {
+    setText(payload);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: isWork ? theme.white : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !isWork ? theme.white : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onChangeText={onChangeText}
+        placeholder={isWork ? 'Add a to do' : 'Where you want to go?'}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -14,8 +60,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: '600',
+  },
+  input: {
+    paddingHorizontal: 18,
+    paddingVertical: 18,
+    backgroundColor: theme.white,
+    marginTop: 20,
+    borderRadius: 15,
+    fontSize: 17,
   },
 });
